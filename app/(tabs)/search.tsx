344466148a0b9aeb808524fb41e2b4c10ccd4806
taejin-k <tj202304@gmail.com>
2026-01-01 22:41:28 +0900

    chore: 사용하지 않는 useSafeAreaInsets 훅 제거

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,7 @@
 import Wrapper from "@/components/common/Wrapper";
 import { Text } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Search = () => {
-  const insets = useSafeAreaInsets();
-
   return (
     <Wrapper>
       <Text>Search Page</Text>
